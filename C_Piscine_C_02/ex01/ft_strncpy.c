fd65596a2ce0a4a408305c7e429a2891bc0a12b4
nadarm <ccc7497@gmail.com>
2020-01-26 16:45:21 +0900

    rush00 and c02 ex01

--- C_Piscine_C_02/ex01/ft_strncpy.c
+++ C_Piscine_C_02/ex01/ft_strncpy.c
@@ -6,18 +6,26 @@
 /*   By: jko <marvin@42.fr>                         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/24 11:24:53 by jko               #+#    #+#             */
-/*   Updated: 2020/01/24 12:49:33 by jko              ###   ########.fr       */
+/*   Updated: 2020/01/26 16:45:02 by jko              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 char	*ft_strncpy(char *dest, char *src, unsigned int n)
 {
 	int nn;
+	int check;
 
+	check = 0;
 	nn = (int)n;
 	while (--nn >= 0)
 	{
-		*(dest + nn) = *(src + nn);
+		if (check == 1 || src[nn] == '\0')
+		{
+			dest[nn] = '\0';
+			check = 1;
+		}
+		else
+			*(dest + nn) = *(src + nn);
 	}
 	return (dest);
 }
