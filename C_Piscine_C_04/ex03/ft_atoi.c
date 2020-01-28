0147153e46f47317662d186b82f65d5348a9446f
nadarm <ccc7497@gmail.com>
2020-01-28 11:18:21 +0900

    c03 ex04 re retry

--- C_Piscine_C_04/ex03/ft_atoi.c
+++ C_Piscine_C_04/ex03/ft_atoi.c
@@ -1,26 +1,15 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jko <marvin@42.fr>                         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/01/27 13:08:23 by jko               #+#    #+#             */
-/*   Updated: 2020/01/28 11:15:11 by jko              ###   ########.fr       */
+/*   Created: 2020/01/28 10:41:24 by jko               #+#    #+#             */
+/*   Updated: 2020/01/28 11:06:42 by jko              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-#include <string.h>
-
-char	*ft_strstr(char *str, char *to_find);
-
-int		main(void)
+int	ft_atoi(char *str)
 {
-	char str[30] = "1";
-	char to_find[10] = "123";
 
-	printf("%s\n%s\n", ft_strstr(str, to_find), strstr(str, to_find));
-
-	return (0);
-}
--- C_Piscine_C_04/ex03/main.c
+++ C_Piscine_C_04/ex03/main.c
@@ -5,34 +5,17 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: jko <marvin@42.fr>                         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/01/27 17:14:17 by jko               #+#    #+#             */
-/*   Updated: 2020/01/28 10:28:00 by jko              ###   ########.fr       */
+/*   Created: 2020/01/28 10:42:16 by jko               #+#    #+#             */
+/*   Updated: 2020/01/28 11:07:24 by jko              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
 #include <unistd.h>
 
-void	ft_putnbr(int nb);
+int	ft_atoi(char *str);
 
-int		main(void)
+int	main(void)
 {
-	ft_putnbr(0);
-	write(1, "\n", 1);
-	ft_putnbr(-2);
-	write(1, "\n", 1);
-	ft_putnbr(1);
-	write(1, "\n", 1);
-	ft_putnbr(11);
-	write(1, "\n", 1);
-	ft_putnbr(-11);
-	write(1, "\n", 1);
-	ft_putnbr(123456789);
-	write(1, "\n", 1);
-	ft_putnbr(-123456789);
-	write(1, "\n", 1);
-	ft_putnbr(2147483647);
-	write(1, "\n", 1);
-	ft_putnbr(-2147483648);
-	write(1, "\n", 1);
 	return (0);
 }
