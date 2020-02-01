6df77d2e0016bbf850264ca2e9cf9ac4f4ad75cd
nadarm <ccc7497@gmail.com>
2020-02-01 21:49:47 +0900

    c07 ex05

--- C_Piscine_C_07/ex05/ft_split.c
+++ C_Piscine_C_07/ex05/ft_split.c
@@ -6,64 +6,106 @@
 /*   By: jko <marvin@42.fr>                         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/30 18:37:28 by jko               #+#    #+#             */
-/*   Updated: 2020/01/31 15:07:35 by jko              ###   ########.fr       */
+/*   Updated: 2020/02/01 21:47:40 by jko              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include <stdio.h>
 
-int		str_cmp(char *s1, char *s2)
+int		str_cmp(char *s1, char *s2, int n)
 {
+	int i;
 
+	i = 0;
+	while (s1[n + i] && s2[i] && s1[n + i] == s2[i])
+		i++;
+	if (s2[i] == 0)
+		return (1);
+	return (0);
+}
+
+int		get_count(char *str, char *charset, int len)
+{
+	int i;
+	int count;
+
+	count = 0;
+	i = 0;
+	if (!str_cmp(str, charset, i++))
+		count++;
+	while (str[i])
+	{
+		if (str_cmp(str, charset, i))
+		{
+			count++;
+			i += len - 1;
+		}
+		i++;
+	}
+	return (count);
+}
+
+void	str_cpy(int *index, char **result, char *str)
+{
+	int i;
+
+	result[index[1]] = (char *)malloc(sizeof(char) * (index[3] - index[2] + 1));
+	i = 0;
+	while (index[2] + i < index[3])
+	{
+		result[index[1]][i] = str[index[2] + i];
+		i++;
+	}
+	result[index[1]][i] = 0;
+	index[1]++;
+}
+
+void	init_arr(void *arr, int size, int type)
+{
+	char	**temp1;
+	int		*temp2;
+	int		i;
+
+	i = 0;
+	if (type == 0)
+	{
+		temp1 = (char **)arr;
+		while (i < size)
+			temp1[i++] = 0;
+	}
+	else
+	{
+		temp2 = (int *)arr;
+		while (i < size)
+			temp2[i++] = 0;
+	}
 }
 
 char	**ft_split(char *str, char *charset)
 {
 	char	**result;
-	int		len;
-	int		i;
-	int		j;
-	int		count;
-	char	*copy_str;
+	int		index[4];
 
-	len = 0;
-	while (str[len])
-		len++;
-	i = 0;
-	copy_str = (char *)malloc(sizeof(char) * (len + 2));
-	while (str[i])
-		copy_str[i++] = str[i];
-	copy_str[i++] = 0;
-	copy_str[i] = -1;
-
-	len = 0;
-	while (charset[len])
-		len++;
-	i = 0;
-	while (copy_str[i] > 0)
-	{
-		if (str_cmp(&copy_str[i], charset) == 0)
+	index[0] = 0;
+	while (charset[index[0]])
+		index[0]++;
+	index[1] = get_count(str, charset, index[0]);
+	result = (char **)malloc(sizeof(char *) * (index[1] + 1));
+	init_arr(result, index[1] + 1, 0);
+	result[index[1]] = 0;
+	init_arr(index + 1, 3, 1);
+	while (str[index[3]])
+		if (str_cmp(str, charset, index[3]))
 		{
-			count++;
-			j = i + len - 1;
-			while (i < j)
-				copy_str[i++] = 0;
+			if (index[2] < index[3])
+				str_cpy(index, result, str);
+			index[3] += index[0];
+			index[2] = index[3];
 		}
-		i++;
-	}
-
-	result = (char **)malloc(sizeof(char *) * (len + 1));
-	result[len] = 0;
-	i = 0;
-
-	for(int k = 0; copy_str[k] > 0; k++)
-	{
-		if (copy_str[k])
-			printf("%c", copy_str[k]);
 		else
-			printf("\n", copy_str[k]);
-	}
-
-	return (0);
+			index[3]++;
+	if (index[2] != index[3])
+		str_cpy(index, result, str);
+	return (result);
 }
--- C_Piscine_C_07/ex05/main.c
+++ C_Piscine_C_07/ex05/main.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-char *ft_split(char *str, char *charset);
+char **ft_split(char *str, char *charset);
 
 int main(void)
 {
-	char str[500] = "12 34 56 78 90";
-	char charset[500] = " ";
+	char str[500] = "12aa34aa56a78aa90";
+	char charset[500] = "aa";
 	char **result = ft_split(str, charset);
-	printf("%s\n%s\n", str, charset);
-//	for(int i = 0; result[i]; i++)
-//		printf("%s\n", result[i]);
+	printf("%s\n%s\n--------------\n", str, charset);
+	for(int i = 0; result[i]; i++)
+		printf("%s\n", result[i]);
 
 	return 0;
 }
